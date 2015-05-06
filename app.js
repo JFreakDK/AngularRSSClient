c82459c65937393147eee3a3513210b8a710173a
JFreakDK <kresten@vester.org>
2015-05-06 22:04:06 +0200

    Working on the login dialog

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 /// <reference path="typings/angularjs/angular.d.ts"/>
 (function () {
-  var app = angular.module('rssReader', [])
-    .value('url', 'https://home.vester.org/selfoss')
+  angular.module('rssReader', [])
+    .value('url', '/selfoss')
     .controller('LoginController', function ($scope, $http, url) {
-    $http.get(url + '/login?username=')
-      .then(function (response) {
-      $scope.question = response.data;
-    }, function (response) {
-        $scope.notice = response.status + " " + response.data.error;
-      });
+    $scope.submit = function () {
+      $http.get(url + '/login', { params: { username: $scope.username, password: $scope.password } })
+        .then(function (response) {
+        $scope.question = response.data;
+      }, function (response) {
+          $scope.notice = response.status + " " + response.data.error;
+        });
+    };
   });
 })();
