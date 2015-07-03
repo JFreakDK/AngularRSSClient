6fe3b3b4d30cf66f7306b074f0cb56d027206524
JFreakDK <kresten@vester.org>
2015-07-03 12:40:31 +0200

    Adding ui-router to start learning this.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-route.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-sanitize.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js"></script>
     <script src="ui-bootstrap-tpls-0.13.0.min.js"></script>
     <script src="app.js"></script>
   </head>
