88f325694b87bb628a1b8716ba85dc3dc24a9a49
JFreakDK <kresten@vester.org>
2015-07-07 10:18:41 +0200

    Transitioned from ngRoute to ui-router

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
 /// <reference path="typings/angularjs/angular.d.ts"/>
 (function () {
-  var app = angular.module('rssReader', ['ngAnimate', 'ngRoute', 'ngSanitize', 'ui.bootstrap']);
+  var app = angular.module('rssReader', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.router']);
   app.value('feedType', 'unread');
   app.value('url', '/selfoss');
-  app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-      controller: 'LoginController',
-      templateUrl: '/AngularRSSClient/views/login.php'
-    })
-      .when('/main', {
-      controller: 'FeedController',
-      templateUrl: '/AngularRSSClient/views/main.php'
-    })
-      .when('/404', {
-      templateUrl: '/AngularRSSClient/views/404.html'
-    })
-      .otherwise({
-      redirectTo: '/404'
-    });
-  });
-  app.config(function ($locationProvider) {
-    $locationProvider.hashPrefix('!');
-  });
-  app.controller('LoginController', function ($scope, $http, $location, url, credentials) {
+
+  app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when('','/');
+    $urlRouterProvider.otherwise('/404');
+
+    $stateProvider
+      .state('login', {
+        url: '/',
+        templateUrl: '/AngularRSSClient/views/login.php',
+        controller: 'LoginController'
+      })
+      .state('main', {
+        url: '/main',
+        controller: 'FeedController',
+        templateUrl: '/AngularRSSClient/views/main.php'
+      })
+      .state('404', {
+        url: '/404',
+        templateUrl: '/AngularRSSClient/views/404.html'
+      });
+  }]);
+
+  app.controller('LoginController', function ($scope, $http, $state, url, credentials) {
     $scope.submit = function () {
       $http.get(url + '/login', { params: { username: $scope.username, password: $scope.password } })
         .then(function (response) {
-        if (response.data.success) {
-          credentials.username = $scope.username;
-          credentials.password = $scope.password;
-          $location.path('/main');
-        } else {
-          $scope.question = 'Wrong';
-        };
-      }, function (response) {
+          if (response.data.success) {
+            credentials.username = $scope.username;
+            credentials.password = $scope.password;
+            $state.go('main');
+          } else {
+            $scope.question = 'Wrong';
+          };
+        }, function (response) {
           $scope.notice = response.status + " " + response.data.error;
         });
     };
   });
+
   app.controller('FeedController', function ($scope, $http, $log, url, feedType, credentials) {
     $scope.items = {};
     $scope.load = function () {
@@ -49,8 +52,8 @@
       $scope.fType = feedType;
       $http.get(url + '/items', parameters)
         .then(function (response) {
-        $scope.items = response.data;
-      }, function (response) {
+          $scope.items = response.data;
+        }, function (response) {
           $scope.notice = response.status + " " + response.data.error;
         });
     };
@@ -60,8 +63,8 @@
       if (item.unread === '1') {
         $http.post(url + '/mark/' + item.id, { params: { username: credentials.username, password: credentials.password } })
           .then(function (response) {
-          item.unread = '0';
-        }, function (response) {
+            item.unread = '0';
+          }, function (response) {
             $scope.notice = response.status + " " + response.data.error;
           });
       };
@@ -71,6 +74,7 @@
       $scope.load();
     };
   });
+
   app.service('credentials', function () {
     this.username = null;
     this.password = null;
--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="app.css" />
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-route.min.js"></script>
+    <!--<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-route.min.js"></script>-->
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-sanitize.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js"></script>
-    <script src="ui-bootstrap-tpls-0.13.0.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.0/ui-bootstrap-tpls.min.js"></script>
     <script src="app.js"></script>
   </head>
   
@@ -20,7 +20,7 @@
         <em>RSS Reader</em>
       </h3>
     </div>
-    <div ng-view></div>
+    <div ui-view></div>
   </body>
 
 </html>
